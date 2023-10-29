4a05b9473ea0dee4df2b1e15988bbb3a3a54e14c
Certifi-Cat <136530777+Certifi-Cat@users.noreply.github.com>
2023-10-29 11:14:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementsByTagName('head')[0].appendChild(script);
 
 const messages = ["Today is a good day to eat peas :)", "What's your favorite type of duck? I like mallards.", "You got this! ", "Can't think of a good pun :(", "If you can dream it, you can do it!", "Consult not your fears but your hopes and your dreams. Think not about your frustrations, but about your unfulfilled potential. Concern yourself not with what you tried and failed in, but with what it is still possible for you to do.", "i like sandwiches"];
 
-const hats = ["hat1.png", "hat2.png", "hat3.png", "hat4.png", "hat5.png"];
+const hats = ["hat1.png", "hat2.png", "hat3.png", "hat4.png", "hat5.png", "hat6.png"];
 
 $(document).ready(generateMessage());
 
